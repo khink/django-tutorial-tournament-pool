2bc4d8af1bc035cb96c76b05466125914051d477
Kees Hink <kees@fourdigits.nl>
2022-11-10 16:29:36 +0100

    List tournaments in index view, link to detail view

--- tournaments/views.py
+++ tournaments/views.py
@@ -1,5 +1,24 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Tournament
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the tournaments index.")
+    tournaments = Tournament.objects.all()
+    template = loader.get_template("tournaments/index.html")
+    context = {
+        "tournament_list": tournaments,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, tournament_id):
+    tournament = Tournament.objects.get(id=tournament_id)
+    teams = tournament.team_set.all()
+    template = loader.get_template("tournaments/detail.html")
+    context = {
+        "tournament_name": tournament.name,
+        "team_list": teams,
+    }
+    return HttpResponse(template.render(context, request))
