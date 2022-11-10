151958c9c7d9d2a0ffd2cb65d9bea205886a5e50
Kees Hink <kees@fourdigits.nl>
2022-11-10 17:00:27 +0100

    Add Prediction model, register in admin

--- predictions/models.py
+++ predictions/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from tournaments.models import Team, Tournament
+
+
+class Prediction(models.Model):
+    name = models.CharField(max_length=200)
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+    winning_team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} predicts {self.winning_team} will win {self.tournament}"
--- tournament_pool/settings.py
+++ tournament_pool/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "tournaments.apps.TournamentsConfig",
+    "predictions.apps.PredictionsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
