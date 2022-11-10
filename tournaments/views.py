2908ec0297ff534eb4f836f826742bff3c2a7d5b
Kees Hink <kees@fourdigits.nl>
2022-11-10 16:35:20 +0100

    Use render() shortcut

--- tournaments/views.py
+++ tournaments/views.py
@@ -1,24 +1,21 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Tournament
 
 
 def index(request):
     tournaments = Tournament.objects.all()
-    template = loader.get_template("tournaments/index.html")
     context = {
         "tournament_list": tournaments,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "tournaments/index.html", context)
 
 
 def detail(request, tournament_id):
     tournament = Tournament.objects.get(id=tournament_id)
     teams = tournament.team_set.all()
-    template = loader.get_template("tournaments/detail.html")
     context = {
         "tournament_name": tournament.name,
         "team_list": teams,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "tournaments/detail.html", context)
