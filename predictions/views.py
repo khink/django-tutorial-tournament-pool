e8e3c8ef88bb45a714e8a1fb69ac337329d8bf24
Kees Hink <kees@fourdigits.nl>
2022-11-10 17:33:32 +0100

    Save a Prediction on form submit

--- predictions/views.py
+++ predictions/views.py
@@ -1,7 +1,10 @@
-from django.http import Http404
+from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render
+from django.urls import reverse
 
-from tournaments.models import Tournament
+from tournaments.models import Team, Tournament
+
+from .models import Prediction
 
 
 def create(request):
@@ -9,9 +12,24 @@ def create(request):
         tournament = Tournament.objects.last()
     except Tournament.DoesNotExist:
         raise Http404("No tournament exists. Create one in the admin.")
-    teams = tournament.team_set.all()
-    context = {
-        "tournament_name": tournament.name,
-        "team_list": teams,
-    }
-    return render(request, "predictions/prediction_form.html", context)
+
+    try:
+        selected_team = tournament.team_set.get(pk=request.POST["team"])
+    except (KeyError, Team.DoesNotExist):
+        # Redisplay the form.
+        teams = tournament.team_set.all()
+        return render(
+            request,
+            "predictions/prediction_form.html",
+            {
+                "tournament_name": tournament.name,
+                "team_list": teams,
+                "error_message": "You didn't pick a team yet.",
+            },
+        )
+    else:
+        prediction = Prediction(tournament=tournament, winning_team=selected_team)
+        prediction.save()
+
+        # We're just going to redirect to the tournaments list for now.
+        return HttpResponseRedirect(reverse("tournaments:index"))
