454bacd0a112421775fbc3671d7d34dd8106c7f8
Kees Hink <kees@fourdigits.nl>
2022-11-12 22:31:31 +0100

    Test models' string representation
    Whether string representations should be tested at all can be
    discussed, but we don't have much else to test.

--- predictions/tests.py
+++ predictions/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from tournaments.models import Team, Tournament
+from .models import Prediction
+
+
+class PredictionModelTests(TestCase):
+    def test_string_representation(self):
+        """
+        The string representation should include name, tournament and winning team.
+        """
+        tournament = Tournament(name="Soccer Worlds 2022")
+        team = Team(name="Netherlands", tournament=tournament)
+        prediction = Prediction(name="Joe", tournament=tournament, winning_team=team)
+        self.assertIn("Joe", str(prediction))
+        self.assertIn("Netherlands", str(prediction))
+        self.assertIn("Soccer Worlds 2022", str(prediction))
--- tournaments/tests.py
+++ tournaments/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Team, Tournament
+
+
+class TournamentModelTests(TestCase):
+    def test_string_representation(self):
+        """
+        The string representation should include name.
+        """
+        tournament = Tournament(name="Soccer Worlds 2022")
+        self.assertIn("Soccer Worlds 2022", str(tournament))
+
+
+class TeamModelTests(TestCase):
+    def test_string_representation(self):
+        """
+        The string representation should include name.
+        """
+        team = Team(name="Netherlands")
+        self.assertIn("Netherlands", str(team))
