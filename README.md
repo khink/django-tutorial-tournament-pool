80f210138f0b5f56da2495237f29d842a3d69c3f
Kees Hink <kees@fourdigits.nl>
2022-11-13 11:56:50 +0100

    Test submitting the prediction form, using Webtest
    Django's TestCase is unsuitable for testing form submit. Yes, it's
    possible to craft a POST request, but it's not guaranteed that a
    form will actually submit the same data.
    
    Webtest allows user-like interaction by clicking on a form and
    submitting it. In my opinion this is a must for comprehensive
    testing.
    
    In this commit we introduce the third parameter to self.assert...()
    calls: this is the message that will be displayed on test failure.
    Use it to describe why your test fails. This is a better place to
    explain your test than a comment line.
    
    As we require a new dependency (django-webtest), we need a place to
    put these dependencies in: requirements.txt. Later, we might make
    this more sophisticated to allow version pinning and upgrading
    (pip-tools is a nice, simple tool for this).

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 A Django project letting people predict tournament results.
 
 This is just a way of checking out [Django's tutorial](https://docs.djangoproject.com/en/4.1/intro/) and applying it to something other than Polls, Questions and Choices (or Books and Authors).
+
+
+## Requirements
+
+This project requires Django and some handy add-ons. To install the required Python packages:
+
+    pip install -r requirements.txt
--- predictions/tests.py
+++ predictions/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from django_webtest import WebTest
 
 from tournaments.models import Team, Tournament
 from .models import Prediction
@@ -38,3 +39,35 @@ class PredictionIndexViewTests(TestCase):
         response = self.client.get(reverse("predictions:list"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, str(prediction))
+
+
+class PredictionWebTests(WebTest):
+    def test_cast_prediction(self):
+        """
+        We should be able to cast a prediction.
+        """
+        tournament = Tournament.objects.create(name="Soccer Worlds 2022")
+        team = Team.objects.create(name="Netherlands", tournament=tournament)
+        response = self.app.get(reverse("predictions:vote"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, f"Who will win {tournament}?")
+
+        # Play around with Webtest's form API
+        form = response.form
+        field = form["team"]
+        self.assertEqual(field.options, [(str(team.pk), False, None)])
+        self.assertEqual(field.value, None)
+
+        # Select a team and submit
+        form["team"] = team.pk
+        post_submit_page = form.submit().follow()
+        self.assertEqual(
+            post_submit_page.request.path,
+            reverse("predictions:list"),
+            "We should be redirected to the predictions list",
+        )
+        self.assertEqual(
+            Prediction.objects.count(), 1, "A Prediction should have been created"
+        )
+        prediction = Prediction.objects.get()
+        self.assertEqual(prediction.winning_team, team)
