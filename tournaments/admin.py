8e70ea36ad14d6fc2d1c6740ce3aaf21be0d9e5d
Kees Hink <kees@fourdigits.nl>
2022-11-25 14:01:51 +0100

    In the admin, show Teams only under Tournaments

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import Team, Tournament
 
-admin.site.register(Team)
-admin.site.register(Tournament)
+
+class TeamInline(admin.StackedInline):
+    model = Team
+    extra = 3
+
+
+class TournamentAdmin(admin.ModelAdmin):
+    inlines = [TeamInline]
+
+
+admin.site.register(Tournament, TournamentAdmin)
